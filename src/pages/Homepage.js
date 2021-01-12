22dad59dec33f8862227268e402270dcebce0a07
Nishan g c <nissangc1996@gmail.com>
2021-01-12 13:40:51 +0545

    first code

---
+++ src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+import Directory from "../components/directory/Directory";
+const homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default homepage;
